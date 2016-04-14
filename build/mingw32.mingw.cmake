560c78e386a16b30adb2fe492adaed6579500624
Florin <florin@libertv.ro>
2016-04-14 13:57:01 +0200

    Windows build environment

--- build/mingw32.mingw.cmake
+++ build/mingw32.mingw.cmake
@@ -4,8 +4,8 @@
 
 set (CMAKE_SYSTEM_NAME "Windows")
 
-set (CROSS C:/Qt/Tools/mingw491_32)
-set (CROSSQT C:/Qt/5.4/mingw491_32)
+set (CROSS F:/Qt/Tools/mingw491_32)
+set (CROSSQT F:/Qt/5.4/mingw491_32)
 
 set (CMAKE_C_COMPILER     ${CROSS}/bin/gcc.exe)
 set (CMAKE_CXX_COMPILER   ${CROSS}/bin/g++.exe)
